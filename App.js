a9a2981f0b3f58b01389119a9a26e2b138cc4dfb
youcefker <youcefdev132@gmail.com>
2021-01-04 13:28:14 +0100

    finish game screen

--- App.js
+++ App.js
@@ -1,15 +1,31 @@
-import React from 'react';
+import React, {useState} from 'react';
 import { StyleSheet, Text, View } from 'react-native';
 import Header from './components/Header'
 import StartGameScreen from './screens/StartGameScreen'
 import GameScreen from './screens/GameScreen'
+import GameOverScreen from './screens/GameOverScreen'
 
 export default function App() {
+  const [userNumber, setUserNumber] = useState()
+  const [guessRounds, setGuessRounds] = useState(0)
+
+  const startGameHandler = (selectedNumber) => {
+    setUserNumber(selectedNumber)
+    setGuessRounds(0)
+  }
+  const gameOverHandler = numOfRounds => {
+    setGuessRounds(numOfRounds)
+  }
+  let content = <StartGameScreen onStartGame={startGameHandler}/>
+
+  if(userNumber) {
+    content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler}/>
+  }
+
   return (
     <View style={styles.screen}>
       <Header title="Guess App"/>
-      <StartGameScreen />
-      <GameScreen />
+      {content}
     </View>
   );
 }
