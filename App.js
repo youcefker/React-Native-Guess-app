5c3b1431a53cba70af0b237a5cd34a7090d95de6
youcefker <youcefdev132@gmail.com>
2021-01-05 13:04:38 +0100

    finishing the game logic

--- App.js
+++ App.js
@@ -9,6 +9,11 @@ export default function App() {
   const [userNumber, setUserNumber] = useState()
   const [guessRounds, setGuessRounds] = useState(0)
 
+  const configureNewGameHandler = () => {
+    setGuessRounds(0)
+    setUserNumber(null)
+  }
+
   const startGameHandler = (selectedNumber) => {
     setUserNumber(selectedNumber)
     setGuessRounds(0)
@@ -18,8 +23,10 @@ export default function App() {
   }
   let content = <StartGameScreen onStartGame={startGameHandler}/>
 
-  if(userNumber) {
+  if(userNumber && guessRounds <= 0) {
     content = <GameScreen userChoice={userNumber} onGameOver={gameOverHandler}/>
+  } else if (guessRounds > 0) {
+    content = <GameOverScreen roundsNumber={guessRounds} userNumber={userNumber} onRestart={configureNewGameHandler}/>
   }
 
   return (
